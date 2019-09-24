9432d9680be3493ab3e1b675ff3fc44274c16d5f
梁柱 <ymblender@gmail.com>
2019-09-24 13:36:19 +0800

    1

---
+++ es/mobile.d.ts
@@ -0,0 +1,7 @@
+export declare const isAndroid: () => boolean;
+export declare const isTablet: () => boolean;
+export declare const isIos: () => boolean;
+export declare const isWechat: () => boolean;
+export declare const isPc: () => boolean;
+export declare const setMobileTouch: () => void;
+export declare const setMobileScroll: (view: any) => void;
